571946fb0042ebc411dbf942e42051fa230bb2ba
Julian Poyourow <julianpoyo@gmail.com>
2018-02-18 20:18:53 +0000

    Added login to sidebar

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -5,6 +5,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { LoginPage } from '../pages/login/login';
 
 @Component({
   templateUrl: 'app.html'
@@ -22,7 +23,8 @@ export class MyApp {
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'List', component: ListPage },
+      { title: 'Login', component: LoginPage }
     ];
 
   }
