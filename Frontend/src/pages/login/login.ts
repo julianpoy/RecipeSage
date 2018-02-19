606eec9dec05c4d05e52adc2f5680a630ea1842d
Hannah Poyourow <hjplsc@gmail.com>
2018-02-18 16:07:04 -0800

    Login/signup page graphics
    Created visual representation for login and signup page and the transition between.

--- Frontend/src/pages/login/login.ts
+++ Frontend/src/pages/login/login.ts
@@ -15,11 +15,30 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class LoginPage {
 
+  showLogin: Boolean;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+  	// code in here will run whenver the component becomes existing
+    this.showLogin = true;
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
+    // code in here will run whenver the component loads
+  }
+
+  toggleLogin() {
+    // when an "instead" button is clicked. Alters visibility of elements.
+    this.showLogin = !this.showLogin;
+  }
+
+  auth() {
+    if (this.showLogin) {
+      console.log("Do login here")
+    } else {
+      console.log("Do signup here")
+    }
+    // what occurs when "login" button is clicked.
   }
 
 }
