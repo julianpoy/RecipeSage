00edeeddcf16cece5d18ae5b0d67ae29129f0d0a
Julian Poyourow <julianpoyo@gmail.com>
2018-02-23 18:52:45 +0000

    Updated electron install command

--- reqs.sh
+++ reqs.sh
@@ -2,4 +2,4 @@
 
 sudo apt install xvfb libasound2 libgconf-2-4 libgtk2.0-0 libxss1 ttf-freefont -y
 
-sudo npm install -g electron-prebuilt
+sudo npm install -g electron --unsafe-perm=true --allow-root
