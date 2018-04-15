b7ca02a83725352fcc5e4e6eb809b1141488a977
Julian Poyourow <julianpoyo@gmail.com>
2018-04-15 03:17:29 +0000

    New staging deploy script

---
+++ deploy-staging.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd Frontend
+
+ionic build --prod
+
+if [ $? -eq 0 ]; then
+    echo OK
+    
+    ssh julian@kondeo.com 'cd /var/www/staging.recipesage.com; rm -rf ./*; cd ~/Projects/chefbook-staging; git pull; cd Backend; npm install; forever restartall'
+
+    cp ./src/assets/splash-353b48.png ./platforms/browser/www/screen
+
+    scp -r ./platforms/browser/www/* julian@kondeo.com:/var/www/staging.recipesage.com
+else
+    echo FAIL
+fi
