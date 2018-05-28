aed7b06008778174cd8da24d099d400b55a26f58
Julian Poyourow <julianpoyo@gmail.com>
2018-05-28 04:52:12 +0000

    All new pages now have priority: low

--- Frontend/src/pages/settings/settings.ts
+++ Frontend/src/pages/settings/settings.ts
@@ -8,7 +8,9 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * Ionic pages and navigation.
  */
 
-@IonicPage()
+@IonicPage({
+  priority: 'low'
+})
 @Component({
   selector: 'page-settings',
   templateUrl: 'settings.html',
