8b3d8b938756a1a5625fd66d3d7fb1fa0ac706c6
Julian Poyourow <julianpoyo@gmail.com>
2018-06-04 01:13:39 +0000

    Great implementation with timers of loadingService - But wrong
    ngx-loading-bar already handles queue of requests

--- Frontend/src/providers/loading-service/loading-service.ts
+++ Frontend/src/providers/loading-service/loading-service.ts
@@ -5,24 +5,32 @@ import { LoadingBarService } from '@ngx-loading-bar/core';
 @Injectable()
 export class LoadingServiceProvider {
 
+  WAIT_TIMEOUT: number = 200;
+
   waiting: any = [];
+  timeout: any;
 
   constructor(private loadingBar: LoadingBarService) {
     console.log('Hello LoadingServiceProvider Provider');
   }
   
   start() {
-    this.loadingBar.start();
-    console.log("starting!")
     
     var me = this;
-    var id = Math.random();
+    var id = Date.now();
+    if (me.waiting.length === 0) this.loadingBar.start();
     me.waiting.push(id);
+    console.log("added!", this.waiting.length)
     return {
       dismiss: function() {
         var idx = me.waiting.indexOf(id);
         me.waiting.splice(idx, 1);
-        me.checkWaiting.call(me);
+        if (me.timeout) clearTimeout(me.timeout);
+        console.log("scheduled removal!", me.waiting.length)
+        me.timeout = setTimeout(function() {
+          console.log("removed!", me.waiting.length)
+          me.checkWaiting.call(me);
+        }, me.WAIT_TIMEOUT);
       }
     }
   }
