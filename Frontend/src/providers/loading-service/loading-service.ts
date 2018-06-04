8cfd1856e5bcc9b53ed6b9ce7b95511c4b10647c
Julian Poyourow <julianpoyo@gmail.com>
2018-06-04 01:16:47 +0000

    Properly utilized ngx-loading-bar (simple)

--- Frontend/src/providers/loading-service/loading-service.ts
+++ Frontend/src/providers/loading-service/loading-service.ts
@@ -5,39 +5,18 @@ import { LoadingBarService } from '@ngx-loading-bar/core';
 @Injectable()
 export class LoadingServiceProvider {
 
-  WAIT_TIMEOUT: number = 200;
-
-  waiting: any = [];
-  timeout: any;
-
   constructor(private loadingBar: LoadingBarService) {
     console.log('Hello LoadingServiceProvider Provider');
   }
   
   start() {
-    
+    this.loadingBar.start();
+
     var me = this;
-    var id = Date.now();
-    if (me.waiting.length === 0) this.loadingBar.start();
-    me.waiting.push(id);
-    console.log("added!", this.waiting.length)
     return {
       dismiss: function() {
-        var idx = me.waiting.indexOf(id);
-        me.waiting.splice(idx, 1);
-        if (me.timeout) clearTimeout(me.timeout);
-        console.log("scheduled removal!", me.waiting.length)
-        me.timeout = setTimeout(function() {
-          console.log("removed!", me.waiting.length)
-          me.checkWaiting.call(me);
-        }, me.WAIT_TIMEOUT);
+        me.loadingBar.complete();
       }
-    }
-  }
-  
-  checkWaiting() {
-    if (this.waiting.length === 0) {
-      this.loadingBar.complete();
     }
   }
 }
