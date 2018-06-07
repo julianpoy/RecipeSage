652c6c69e7a1da3851a70569ef00e57e764344d3
Julian Poyourow <julianpoyo@gmail.com>
2018-06-07 16:46:37 +0000

    Updated deployment scripts for updated build process

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -2,16 +2,14 @@
 
 cd Frontend
 
-ionic build --prod
+npm run dist
 
 if [ $? -eq 0 ]; then
     echo OK
     
     ssh julian@kondeo.com 'cd /var/www/recipesage.com; rm -rf ./*; cd ~/Projects/chefbook; git checkout Backend/package-lock.json; git pull; cd Backend; npm install; forever restart chefbook'
 
-    cp ./src/assets/transparent-square.png ./platforms/browser/www/screen
-
-    scp -r ./platforms/browser/www/* julian@kondeo.com:/var/www/recipesage.com
+    scp -r ./www/* julian@kondeo.com:/var/www/recipesage.com
 else
     echo FAIL
 fi
