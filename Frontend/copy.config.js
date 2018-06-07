b5611759eb4dafbfeb96775604a930f362923b7c
Julian Poyourow <julianpoyo@gmail.com>
2018-06-07 16:45:51 +0000

    Added purifyCSS to build process & removed unused Ionic components

--- Frontend/copy.config.js
+++ Frontend/copy.config.js
@@ -2,7 +2,9 @@ module.exports = {
   copyWorkbox: {
   src: ['./node_modules/workbox-sw/build/workbox-sw.js', 
         './node_modules/workbox-core/build/workbox-core.prod.js', 
-        './node_modules/workbox-precaching/build/workbox-precaching.prod.js'],
+        './node_modules/workbox-precaching/build/workbox-precaching.prod.js',
+        './node_modules/workbox-routing/build/workbox-routing.prod.js',
+        './node_modules/workbox-strategies/build/workbox-strategies.prod.js'],
 	dest: '{{WWW}}/workbox-3.2.0'
   }
 }
