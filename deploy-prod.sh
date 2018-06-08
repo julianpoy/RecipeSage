420649bdbe14341351b9272285fef6e897bfc322
Julian Poyourow <julianpoyo@gmail.com>
2018-06-08 10:10:25 -0700

    Updated deploy scripts for new server configuration

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -7,9 +7,9 @@ npm run dist
 if [ $? -eq 0 ]; then
     echo OK
     
-    ssh julian@kondeo.com 'cd /var/www/recipesage.com; rm -rf ./*; cd ~/Projects/chefbook; git checkout Backend/package-lock.json; git pull; cd Backend; npm install; forever restart chefbook'
+    ssh julian@recipesage.com 'cd /var/www/recipesage.com; rm -rf ./*; cd ~/Projects/chefbook; git checkout Backend/package-lock.json; git pull; cd Backend; npm install; forever restart chefbook'
 
-    scp -r ./www/* julian@kondeo.com:/var/www/recipesage.com
+    scp -r ./www/* julian@recipesage.com:/var/www/recipesage.com
 else
     echo FAIL
 fi
--- deploy-staging.sh
+++ deploy-staging.sh
@@ -7,9 +7,9 @@ npm run dist
 if [ $? -eq 0 ]; then
     echo OK
     
-    ssh julian@kondeo.com 'cd /var/www/staging.recipesage.com; rm -rf ./*; cd ~/Projects/chefbook-staging; git checkout Backend/package-lock.json; git pull; cd Backend; npm install; forever restart chefbook-staging'
+    ssh julian@staging.recipesage.com 'cd /var/www/staging.recipesage.com; rm -rf ./*; cd ~/Projects/chefbook-staging; git checkout Backend/package-lock.json; git pull; cd Backend; npm install; forever restart chefbook-staging'
 
-    scp -r ./www/* julian@kondeo.com:/var/www/staging.recipesage.com
+    scp -r ./www/* julian@staging.recipesage.com:/var/www/staging.recipesage.com
 else
     echo FAIL
 fi
