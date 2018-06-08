f4649878f697df230d302077289f7038a7315234
Julian Poyourow <julianpoyo@gmail.com>
2018-06-08 13:17:04 -0700

    Removed some necessary components from SASS blacklist

--- Frontend/sass.config.js
+++ Frontend/sass.config.js
@@ -63,7 +63,7 @@ module.exports = {
    */
   excludeFiles: [
     /*  /\.(wp).(scss)$/i  */
-    /(action-sheet|checkbox|chip|datetime|img|infinite-scroll|loading|note|picker|radio|range|segment|select|show-hide-when|slides|tabs|virtual-scroll|cordova)/i,
+    /(action-sheet|checkbox|chip|datetime|infinite-scroll|loading|note|picker|range|segment|select|show-hide-when|slides|tabs|cordova)/i,
   ],
 
   /**
