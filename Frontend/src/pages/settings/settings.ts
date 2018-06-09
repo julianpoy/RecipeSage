e3c91dd454f206bc8c92fe372741888922a182cc
Julian Poyourow <julianpoyo@gmail.com>
2018-06-09 10:06:26 -0700

    Added missing alertctrl req

--- Frontend/src/pages/settings/settings.ts
+++ Frontend/src/pages/settings/settings.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController, AlertController } from 'ionic-angular';
 
 @IonicPage({
   priority: 'low'
@@ -13,6 +13,7 @@ export class SettingsPage {
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
+    public toastCtrl: ToastController,
     public alertCtrl: AlertController) {
   }
 
