eeeebda805698c9a816e91e29830b52508442a42
Julian Poyourow <julianpoyo@gmail.com>
2018-06-10 08:21:21 -0700

    Added PM2 config file

---
+++ Backend/ecosystem.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+    {
+      name      : 'RecipeSageAPI',
+      script    : 'bin/www',
+      instances : "2",
+      instance_var: "INSTANCE_ID",
+      env: {
+        NODE_ENV: 'production',
+        PORT: '3030'
+      }
+    },
+    {
+      name      : 'RecipeSageStagingAPI',
+      script    : 'bin/www',
+      instances : "2",
+      instance_var: "INSTANCE_ID",
+      env: {
+        PORT: '3040'
+      }
+    }
+  ]
+};
