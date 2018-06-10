6cf0c1032287e38f425d0184c55191d289c858e2
Julian Poyourow <julianpoyo@gmail.com>
2018-06-10 08:47:33 -0700

    Updated reload commands for pm2

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -7,7 +7,7 @@ npm run dist
 if [ $? -eq 0 ]; then
     echo OK
     
-    ssh julian@recipesage.com 'cd /var/www/recipesage.com; rm -rf ./*; cd ~/Projects/chefbook; git checkout Backend/package-lock.json; git pull; cd Backend; npm install; forever restart chefbook'
+    ssh julian@recipesage.com 'cd /var/www/recipesage.com; rm -rf ./*; cd ~/Projects/chefbook; git checkout Backend/package-lock.json; git pull; cd Backend; npm install; pm2 reload RecipeSageAPI'
 
     scp -r ./www/* julian@recipesage.com:/var/www/recipesage.com
 else
--- deploy-staging.sh
+++ deploy-staging.sh
@@ -7,7 +7,7 @@ npm run dist
 if [ $? -eq 0 ]; then
     echo OK
     
-    ssh julian@staging.recipesage.com 'cd /var/www/staging.recipesage.com; rm -rf ./*; cd ~/Projects/chefbook-staging; git checkout Backend/package-lock.json; git pull; cd Backend; npm install; forever restart chefbook-staging'
+    ssh julian@staging.recipesage.com 'cd /var/www/staging.recipesage.com; rm -rf ./*; cd ~/Projects/chefbook-staging; git checkout Backend/package-lock.json; git pull; cd Backend; npm install; pm2 reload RecipeSageStagingAPI'
 
     scp -r ./www/* julian@staging.recipesage.com:/var/www/staging.recipesage.com
 else
