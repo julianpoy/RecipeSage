656ea4f1b805b4c2b792bbc317fe2f5f81172001
Julian Poyourow <julianpoyo@gmail.com>
2018-06-20 22:28:06 -0700

    Replaced pug with jade

--- Backend/app.js
+++ Backend/app.js
@@ -49,7 +49,7 @@ app.use(cookieParser());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
