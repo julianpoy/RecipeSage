74a6792ff6d9c2edf4984ba1693bb4fe654cc064
Julian Poyourow <julianpoyo@gmail.com>
2018-08-13 18:20:20 -0700

    Added NewShoppingListItemModalPage and ShoppingListPage contents

--- Frontend/src/pages/shopping-list-components/new-shopping-list-item-modal/new-shopping-list-item-modal.ts
+++ Frontend/src/pages/shopping-list-components/new-shopping-list-item-modal/new-shopping-list-item-modal.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ViewController, ToastController } from 'ionic-angular';
+import { RecipeServiceProvider } from '../../../providers/recipe-service/recipe-service';
+import { LoadingServiceProvider } from '../../../providers/loading-service/loading-service';
 
 @IonicPage()
 @Component({
@@ -8,22 +10,234 @@ import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angul
 })
 export class NewShoppingListItemModalPage {
 
-  // LABELS REFERENCED HERE ARE ACTUALLY COLLABORATORS ON SHOPPING LIST ITEM
-  labelObjectsByTitle: any = {};
-  existingLabels: any = [];
-  selectedLabels: any = [];
-  pendingLabel: string = '';
+  inputType: string = 'items';
+
+  itemFields: any = [{}];
+
+  searchWorker: any;
+  searchText: string = '';
+  searching: boolean = false;
+
+  selectedRecipe: any;
+
+  recipes: any = [];
+  ingredientBinders: any = {};
   showAutocomplete: boolean = false;
   autocompleteSelectionIdx: number = -1;
 
   constructor(
     public navCtrl: NavController,
     public viewCtrl: ViewController,
+    public recipeService: RecipeServiceProvider,
+    public loadingService: LoadingServiceProvider,
+    public toastCtrl: ToastController,
     public navParams: NavParams) {
+
+  }
+
+  ionViewWillEnter() {
+    var loading = this.loadingService.start();
+
+    this.loadRecipes().then(function () {
+      loading.dismiss();
+    }, function () {
+      loading.dismiss();
+    });
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad NewShoppingListItemModalPage');
+  }
+
+  loadRecipes() {
+    var me = this;
+
+    return new Promise(function (resolve, reject) {
+      me.recipeService.fetch({
+        folder: 'main',
+        sortBy: 'title',
+      }).subscribe(function (response) {
+
+        if (me.searchWorker) me.searchWorker.terminate();
+        me.searchWorker = new Worker('assets/src/search-worker.js');
+
+        me.searchWorker.postMessage(JSON.stringify({
+          op: 'init',
+          data: response
+        }));
+
+        me.searchWorker.onmessage = function (e) {
+          me.searching = false;
+          var message = JSON.parse(e.data);
+          if (message.op === 'results') {
+            me.recipes = message.data;
+          }
+        }
+
+        me.recipes = response;
+
+        resolve();
+      }, function (err) {
+        reject();
+
+        switch (err.status) {
+          case 0:
+            let offlineToast = me.toastCtrl.create({
+              message: 'It looks like you\'re offline. While offline, we\'re only able to fetch data you\'ve previously accessed on this device.',
+              duration: 5000
+            });
+            offlineToast.present();
+            break;
+          case 401:
+            me.navCtrl.setRoot('LoginPage', {}, { animate: true, direction: 'forward' });
+            break;
+          default:
+            let errorToast = me.toastCtrl.create({
+              message: 'An unexpected error occured. Please restart application.',
+              duration: 30000
+            });
+            errorToast.present();
+            break;
+        }
+      });
+    });
+  }
+
+  onSearchInputChange() {
+    this.search(this.searchText);
+  }
+
+  search(text) {
+    if (!text) text = '';
+    this.searchText = text;
+    this.searchWorker.postMessage(JSON.stringify({
+      op: 'search',
+      data: text
+    }));
+    this.searching = true;
+  }
+
+  addOrRemoveTextFields() {
+    if ((this.itemFields[this.itemFields.length - 1].title || '').length > 0) {
+      this.itemFields.push({});
+    }
+  }
+
+  toggleAutocomplete(show, event?) {
+    if (event && event.relatedTarget) {
+      if (event.relatedTarget.className.indexOf('suggestion') > -1) {
+        return;
+      }
+    }
+    this.showAutocomplete = show;
+    this.autocompleteSelectionIdx = -1;
+  }
+
+  labelFieldKeyUp(event) {
+    // Only listen for up or down arrow
+    if (event.keyCode !== 38 && event.keyCode !== 40) return;
+
+    // Get all suggestions (including click to create)
+    var suggestions = document.getElementsByClassName('autocomplete')[0].children;
+
+    // If result list size was reduced, do not overflow
+    if (this.autocompleteSelectionIdx > suggestions.length - 1) this.autocompleteSelectionIdx = suggestions.length - 1;
+
+    if (event.keyCode === 40 && this.autocompleteSelectionIdx < suggestions.length - 1) {
+      // Arrow Down
+      this.autocompleteSelectionIdx++;
+    } else if (event.keyCode === 38 && this.autocompleteSelectionIdx >= 0) {
+      // Arrow Up
+      this.autocompleteSelectionIdx--;
+    }
+
+    if (this.autocompleteSelectionIdx === -1) {
+      (document.getElementById('labelInputField') as HTMLElement).focus();
+    } else {
+      (suggestions[this.autocompleteSelectionIdx] as HTMLElement).focus();
+    }
+  }
+
+  selectRecipe(recipe) {
+    this.searchText = '';
+    this.toggleAutocomplete(false);
+
+    var me = this;
+    this.recipeService.fetchById(recipe._id).subscribe(function (response) {
+      me.selectedRecipe = response;
+      me.ingredientBinders = {};
+
+      if (me.selectedRecipe.ingredients) {
+        me.selectedRecipe.ingredients = me.selectedRecipe.ingredients.match(/[^\r\n]+/g);
+
+        for (var i = 0; i < me.selectedRecipe.ingredients.length; i++) {
+          me.ingredientBinders[i] = true;
+        }
+      }
+    }, function (err) {
+      switch (err.status) {
+        case 0:
+          let offlineToast = me.toastCtrl.create({
+            message: 'It looks like you\'re offline. While offline, we\'re only able to fetch data you\'ve previously accessed on this device.',
+            duration: 5000
+          });
+          offlineToast.present();
+          break;
+        case 401:
+          me.navCtrl.setRoot('LoginPage', {}, { animate: true, direction: 'forward' });
+          break;
+        default:
+          let errorToast = me.toastCtrl.create({
+            message: 'An unexpected error occured. Please restart application.',
+            duration: 30000
+          });
+          errorToast.present();
+          break;
+      }
+    });
+  }
+
+  isFormValid() {
+    if (this.inputType === 'recipe' && this.selectedRecipe && this.selectedRecipe._id) {
+      for (var key in this.ingredientBinders) {
+        if (this.ingredientBinders.hasOwnProperty(key) && this.ingredientBinders[key]) return true;
+      }
+    }
+    if (this.inputType === 'items') {
+      for (var i = 0; i < this.itemFields.length; i++) {
+        if (this.itemFields[i].title) return true;
+      }
+    }
+    return false;
+  }
+
+  save() {
+    var items;
+    if (this.inputType === 'recipe') {
+      items = [];
+      for (var i = 0; i < this.selectedRecipe.ingredients.length; i++) {
+        if (this.ingredientBinders[i]) {
+          items.push({
+            title: this.selectedRecipe.ingredients[i],
+            recipe: this.selectedRecipe._id
+          });
+        }
+      }
+    } else {
+      // Redundant for now. Kept for sterilization
+      items = this.itemFields.filter(function(e){
+        return (e.title || '').length > 0;
+      }).map(function(e) {
+        return {
+          title: e.title
+        };
+      });
+    }
+
+    this.viewCtrl.dismiss({
+      destination: false,
+      items: items
+    });
   }
 
   cancel() {
--- Frontend/src/pages/shopping-list-components/shopping-list/shopping-list.ts
+++ Frontend/src/pages/shopping-list-components/shopping-list/shopping-list.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController, ModalController } from 'ionic-angular';
+import { LoadingServiceProvider } from '../../../providers/loading-service/loading-service';
+import { ShoppingListServiceProvider } from '../../../providers/shopping-list-service/shopping-list-service';
 
 @IonicPage({
   segment: 'shopping-lists/:shoppingListId',
@@ -11,11 +13,220 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ShoppingListPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  shoppingListId: string;
+  list: any = { items: [], collaborators: [] };
+
+  itemsByRecipeId: any = {};
+  recipeIds: any = [];
+
+  lastItemRemoved: any;
+
+  constructor(
+    public navCtrl: NavController,
+    public loadingService: LoadingServiceProvider,
+    public shoppingListService: ShoppingListServiceProvider,
+    public toastCtrl: ToastController,
+    public modalCtrl: ModalController,
+    public navParams: NavParams) {
+
+    this.shoppingListId = navParams.get('shoppingListId');
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ShoppingListPage');
   }
 
+  ionViewWillEnter() {
+    var loading = this.loadingService.start();
+
+    this.loadList().then(function () {
+      loading.dismiss();
+    }, function () {
+      loading.dismiss();
+    });
+  }
+
+  refresh(loader) {
+    this.loadList().then(function () {
+      loader.complete();
+    }, function () {
+      loader.complete();
+    });
+  }
+
+  processIncomingList(list) {
+    var me = this;
+
+    me.list = list;
+
+    var items = (me.list.items || []);
+
+    me.recipeIds = [];
+    me.itemsByRecipeId = {};
+    for (var i = 0; i < items.length; i++) {
+      if (!items[i].recipe) continue;
+
+      var recipeId = items[i].recipe.id;
+
+      if (me.recipeIds.indexOf(recipeId) === -1) me.recipeIds.push(recipeId);
+
+      if (!me.itemsByRecipeId[recipeId]) me.itemsByRecipeId[recipeId] = [];
+      me.itemsByRecipeId[recipeId].push(items[i]);
+    }
+  }
+
+  loadList() {
+    var me = this;
+
+    return new Promise(function (resolve, reject) {
+      me.shoppingListService.fetchById(me.shoppingListId).subscribe(function (response) {
+        me.processIncomingList(response);
+
+        resolve();
+      }, function (err) {
+        switch (err.status) {
+          case 0:
+            let offlineToast = me.toastCtrl.create({
+              message: 'It looks like you\'re offline. While offline, we\'re only able to fetch data you\'ve previously accessed on this device.',
+              duration: 5000
+            });
+            offlineToast.present();
+            break;
+          case 401:
+            me.navCtrl.setRoot('LoginPage', {}, { animate: true, direction: 'forward' });
+            break;
+          case 404:
+            let errorToast = me.toastCtrl.create({
+              message: 'Recipe not found. Does this recipe URL exist?',
+              duration: 30000,
+              dismissOnPageChange: true
+            });
+            errorToast.present();
+            break;
+          default:
+            errorToast = me.toastCtrl.create({
+              message: 'An unexpected error occured. Please restart application.',
+              duration: 30000
+            });
+            errorToast.present();
+            break;
+        }
+
+        reject();
+      });
+    });
+  }
+
+  removeItem(item) {
+    var me = this;
+    var loading = this.loadingService.start();
+
+    this.lastItemRemoved = item;
+
+    this.shoppingListService.remove({
+      _id: this.list._id,
+      items: [ item._id ]
+    }).subscribe(function (response) {
+      loading.dismiss();
+
+      me.processIncomingList(response);
+
+      var toast = me.toastCtrl.create({
+        message: 'Removed: ' + item.title,
+        duration: 5000,
+        showCloseButton: true,
+        closeButtonText: 'Undo',
+      });
+      toast.onDidDismiss((data, role) => {
+        if (role == "close") {
+          // me.undoRemove();
+          me._addItems([ item ]);
+        }
+      });
+      toast.present();
+    }, function (err) {
+      loading.dismiss();
+      switch (err.status) {
+        case 0:
+          me.toastCtrl.create({
+            message: 'It looks like you\'re offline. While offline, all RecipeSage functions are read-only.',
+            duration: 5000
+          }).present();
+          break;
+        case 401:
+          me.toastCtrl.create({
+            message: 'You are not authorized for this action! If you believe this is in error, please log out and log in using the side menu.',
+            duration: 6000
+          }).present();
+          break;
+        default:
+          me.toastCtrl.create({
+            message: 'An unexpected error occured. Please try again.',
+            duration: 6000
+          }).present();
+          break;
+      }
+    });
+  }
+
+  undoRemove() {
+    if (!this.lastItemRemoved) return;
+
+    this._addItems([this.lastItemRemoved]);
+  }
+
+  _addItems(items) {
+    var me = this;
+    var loading = this.loadingService.start();
+
+    this.shoppingListService.addItems({
+      _id: this.list._id,
+      items: items
+    }).subscribe(function (response) {
+      loading.dismiss();
+
+      me.processIncomingList(response);
+    }, function (err) {
+      loading.dismiss();
+      switch (err.status) {
+        case 0:
+          me.toastCtrl.create({
+            message: 'It looks like you\'re offline. While offline, all RecipeSage functions are read-only.',
+            duration: 5000
+          }).present();
+          break;
+        case 401:
+          me.toastCtrl.create({
+            message: 'You are not authorized for this action! If you believe this is in error, please log out and log in using the side menu.',
+            duration: 6000
+          }).present();
+          break;
+        default:
+          me.toastCtrl.create({
+            message: 'An unexpected error occured. Please try again.',
+            duration: 6000
+          }).present();
+          break;
+      }
+    });
+  }
+
+  newShoppingListItem() {
+    var me = this;
+    let modal = this.modalCtrl.create('NewShoppingListItemModalPage');
+    modal.present();
+    modal.onDidDismiss(data => {
+      if (data.items) {
+        this._addItems(data.items);
+      }
+
+      if (!data.destination) return;
+
+      if (data.setRoot) {
+        me.navCtrl.setRoot(data.destination, data.routingData || {}, { animate: true, direction: 'forward' });
+      } else {
+        me.navCtrl.push(data.destination, data.routingData);
+      }
+    });
+  }
 }
