b9a8aa900773866cad63e483b88e8388115b7fc4
Julian Poyourow <julianpoyo@gmail.com>
2018-08-19 12:02:19 -0700

    Added list creation date to ShoppingListsPage

--- Frontend/src/pages/shopping-list-components/shopping-lists/shopping-lists.ts
+++ Frontend/src/pages/shopping-list-components/shopping-lists/shopping-lists.ts
@@ -117,4 +117,32 @@ export class ShoppingListsPage {
       shoppingListId: listId
     });
   }
+
+  formatItemCreationDate(plainTextDate) {
+    var todayAfter = new Date();
+    todayAfter.setHours(0);
+    todayAfter.setMinutes(0);
+    todayAfter.setSeconds(0);
+    todayAfter.setMilliseconds(0);
+
+    var plainTextAfter = new Date();
+    plainTextAfter.setDate(plainTextAfter.getDate() - 7);
+
+    var toFormat = new Date(plainTextDate);
+
+    if (todayAfter < toFormat) {
+      return 'today';
+    }
+
+    if (plainTextAfter < toFormat) {
+      var dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+      return dayNames[toFormat.getDay()];
+    }
+
+    return toFormat.toLocaleString((<any>window.navigator).userLanguage || window.navigator.language, {
+      month: 'numeric',
+      day: 'numeric',
+      year: 'numeric'
+    });
+  }
 }
