7abbb2ede7af8c6df787247651cd92d5263225a9
Julian Poyourow <julianpoyo@gmail.com>
2019-01-07 18:44:52 -0800

    Removed all references to mongodb and mongoose

--- Backend/mongoose-models/db.js
+++
@@ -1,7 +0,0 @@
-var mongoose = require('mongoose');
-var config = require('../config/config.json');
-
-mongoose.connect('mongodb://localhost/' + (config.db || 'chefBook'), {
-  keepAlive: true,
-  reconnectTries: Number.MAX_VALUE
-});
