d3db1c7fc27ac42163691d2a2c62b5b69f1d3302
Julian Poyourow <julianpoyo@gmail.com>
2019-01-27 22:38:22 -0800

    DB connection is now sourced from env variables
    This requires an environment change on staging, however

---
+++ Backend/config/sequelize-config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  "development": {
+    "username": process.env.RS_DEV_DB_USERNAME || "chefbook",
+    "password": process.env.RS_DEV_DB_PASSWORD || "admin",
+    "database": process.env.RS_DEV_DB_DATABASE || "chefbook",
+    "host"    : process.env.RS_DEV_DB_HOST     || "127.0.0.1",
+    "dialect": "postgres",
+    "operatorsAliases": false
+  },
+  "test": {
+    "dialect": "sqlite",
+    "storage": "test.sqlite3",
+    "logging": false,
+    "operatorsAliases": false
+  },
+  "staging": {
+    "username": process.env.RS_STG_DB_USERNAME || "chefbook",
+    "password": process.env.RS_STG_DB_PASSWORD || null,
+    "database": process.env.RS_STG_DB_DATABASE || "chefbook",
+    "host"    : process.env.RS_STG_DB_HOST     || "127.0.0.1",
+    "dialect": "postgres",
+    "operatorsAliases": false
+  },
+  "production": {
+    "username": process.env.RS_PROD_DB_USERNAME || "chefbook",
+    "password": process.env.RS_PROD_DB_PASSWORD || null,
+    "database": process.env.RS_PROD_DB_DATABASE || "chefbook",
+    "host"    : process.env.RS_PROD_DB_HOST     || "127.0.0.1",
+    "dialect": "postgres",
+    "operatorsAliases": false
+  }
+}
