c6a2f5f08fc3928e0ab4477052000aa99061722a
Julian Poyourow <julianpoyo@gmail.com>
2019-02-13 22:22:18 -0800

    Added contribute page

--- Frontend/src/pages/info-components/contribute/contribute.module.ts
+++ Frontend/src/pages/info-components/contribute/contribute.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { ContributePage } from './contribute';
+import { ComponentsModule } from '../../../components/components.module';
 
 @NgModule({
   declarations: [
@@ -8,6 +9,7 @@ import { ContributePage } from './contribute';
   ],
   imports: [
     IonicPageModule.forChild(ContributePage),
+    ComponentsModule
   ],
 })
 export class ContributePageModule {}
