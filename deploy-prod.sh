8c444a48c12c8275f9f5eec02cbfb186f2d02a63
Julian Poyourow <julianpoyo@gmail.com>
2019-02-28 21:58:20 -0800

    Deploy scripts now use tar for frontend

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -6,10 +6,16 @@ npm run dist
 
 if [ $? -eq 0 ]; then
     echo OK
-    
-    ssh julian@recipesage.com 'cd /var/www/recipesage.com; rm -rf ./*; cd ~/Projects/chefbook; git checkout Backend/package-lock.json; git pull; cd Backend; npm install; pm2 reload RecipeSageAPI'
 
-    scp -r ./www/* julian@recipesage.com:/var/www/recipesage.com
+    tar -czf deploy-prod.tgz ./www/*
+
+    ssh julian@recipesage.com 'cd ~/Projects/chefbook; git checkout Backend/package-lock.json; git pull; cd Backend; npm install; pm2 reload RecipeSageAPI'
+
+    scp deploy-prod.tgz julian@recipesage.com:/tmp
+
+    rm deploy-prod.tgz
+
+    ssh julian@recipesage.com 'rm /tmp/deploy-prod.tgz; cd /var/www/recipesage.com; rm -rf ./*; tar -zxvf /tmp/deploy-prod.tgz; mv www/* .'
 else
     echo FAIL
 fi
