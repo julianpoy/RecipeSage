bc5eefa40ea38d143744d36ade043f804253ad33
Julian Poyourow <julianpoyo@gmail.com>
2019-03-02 19:22:57 -0800

    Fixed reference and removal of temp tgz for deployment

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -9,13 +9,13 @@ if [ $? -eq 0 ]; then
 
     tar -czf deploy-prod.tgz ./www/*
 
-    ssh julian@recipesage.com 'cd ~/Projects/chefbook; git checkout Backend/package-lock.json; git pull; cd Backend; npm install; pm2 reload RecipeSageAPI'
+    ssh julian@recipesage.com 'rm /tmp/deploy-prod.tgz; cd ~/Projects/chefbook; git checkout Backend/package-lock.json; git pull; cd Backend; npm install; pm2 reload RecipeSageAPI'
 
     scp deploy-prod.tgz julian@recipesage.com:/tmp
 
     rm deploy-prod.tgz
 
-    ssh julian@recipesage.com 'rm /tmp/deploy-prod.tgz; cd /var/www/recipesage.com; rm -rf ./*; tar -zxvf /tmp/deploy-prod.tgz; mv www/* .'
+    ssh julian@recipesage.com 'cd /var/www/recipesage.com; rm -rf ./*; tar -zxvf /tmp/deploy-prod.tgz; mv www/* .'
 else
     echo FAIL
 fi
--- deploy-staging.sh
+++ deploy-staging.sh
@@ -9,13 +9,13 @@ if [ $? -eq 0 ]; then
 
     tar -czf deploy-staging.tgz ./www/*
 
-    ssh julian@staging.recipesage.com 'cd ~/Projects/chefbook-staging; git checkout Backend/package-lock.json; git pull; cd Backend; npm install; pm2 reload RecipeSageStagingAPI'
+    ssh julian@staging.recipesage.com 'rm /tmp/deploy-staging.tgz; cd ~/Projects/chefbook-staging; git checkout Backend/package-lock.json; git pull; cd Backend; npm install; pm2 reload RecipeSageStagingAPI'
 
     scp deploy-staging.tgz julian@staging.recipesage.com:/tmp
 
     rm deploy-staging.tgz
 
-    ssh julian@staging.recipesage.com 'rm /tmp/deploy-staging.tgz; cd /var/www/staging.recipesage.com; rm -rf ./*; tar -zxvf /tmp/deploy-staging.tgz; mv www/* .'
+    ssh julian@staging.recipesage.com 'cd /var/www/staging.recipesage.com; rm -rf ./*; tar -zxvf /tmp/deploy-staging.tgz; mv www/* .'
 else
     echo FAIL
 fi
