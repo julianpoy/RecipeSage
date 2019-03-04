bf83c8a70a522815d763851eb374cbe1d3e8c2d9
Julian Poyourow <julianpoyo@gmail.com>
2019-03-03 16:53:13 -0800

    Verbump 1.5.6

--- Backend/app.js
+++ Backend/app.js
@@ -22,7 +22,7 @@ var devMode = appConfig.environment === 'dev';
 
 Raven.config(appConfig.sentry.dsn, {
   environment: appConfig.environment,
-  release: '1.5.5'
+  release: '1.5.6'
 }).install();
 
 // Routes
