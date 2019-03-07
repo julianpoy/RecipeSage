e495caaf5692edc3de322aa10853f86a1f440eba
Julian Poyourow <julianpoyo@gmail.com>
2019-03-06 21:33:25 -0800

    Add largeFileOverride

--- Frontend/src/pages/settings-components/import-livingcookbook/import-livingcookbook.ts
+++ Frontend/src/pages/settings-components/import-livingcookbook/import-livingcookbook.ts
@@ -19,6 +19,8 @@ export class ImportLivingcookbookPage {
   loading = null;
   imageFile = null;
 
+  ignoreLargeFiles: boolean;
+
   constructor(
     public navCtrl: NavController,
     public loadingService: LoadingServiceProvider,
@@ -26,6 +28,8 @@ export class ImportLivingcookbookPage {
     public recipeService: RecipeServiceProvider,
     public utilService: UtilServiceProvider,
     public navParams: NavParams) {
+
+    this.ignoreLargeFiles = !!localStorage.getItem("largeFileOverride");
   }
 
   setFile(event) {
@@ -50,7 +54,7 @@ export class ImportLivingcookbookPage {
   }
 
   isFileTooLarge() {
-    if (this.imageFile && this.imageFile.size / 1024 / 1024 > 350) {
+    if (!this.ignoreLargeFiles && this.imageFile && this.imageFile.size / 1024 / 1024 > 350) {
       // File is larger than 350MB
       return true;
     }
