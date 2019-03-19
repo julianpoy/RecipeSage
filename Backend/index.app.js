2f5f35bb303e24756ee96591fa02a02b16b93744
Julian Poyourow <julianpoyo@gmail.com>
2019-03-18 20:32:23 -0700

    Moved indexer logic to separate app

---
+++ Backend/index.app.js
@@ -0,0 +1,43 @@
+let ElasticService = require('./services/elastic');
+let cron = require('node-cron');
+let Raven = require('raven');
+let SQ = require("sequelize");
+let Op = SQ.Op;
+
+var Recipe = require('./models').Recipe;
+
+cron.schedule('*/1 * * * *', () => {
+  let lt = new Date();
+  lt.setDate(lt.getDate() - 7);
+
+  Recipe.findAll({
+    where: {
+      [Op.or]: [
+        { indexedAt: null },
+        { indexedAt: { [Op.lt]: lt } }
+      ]
+    },
+    limit: 400,
+    order: [
+      ['indexedAt', 'ASC']
+    ]
+  }).then(recipes => {
+    if (!recipes || recipes.length === 0) return;
+
+    return ElasticService.bulk('index', 'recipes', recipes).then(() => {
+      let ids = recipes.map(r => r.id);
+      return Recipe.update(
+        { indexedAt: new Date() },
+        {
+          where: {
+            id: { [Op.in]: ids }
+          },
+          silent: true,
+          hooks: false
+        }
+      );
+    });
+  }).catch(e => {
+    Raven.captureException(e);
+  });
+});
