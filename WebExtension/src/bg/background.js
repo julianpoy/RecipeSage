8e0ab2d341f348c3823c9a7411776db6ceea5eff
Julian Poyourow <julianpoyo@gmail.com>
2019-04-11 17:35:56 -0700

    WE background script now refreshes session periodically

--- WebExtension/src/bg/background.js
+++ WebExtension/src/bg/background.js
@@ -25,3 +25,20 @@ chrome.contextMenus.create({
     });
   }
 });
+
+// Extend the current token if it exists
+let renewToken = () => {
+  chrome.storage.local.get(['token'], result => {
+    token = result.token;
+
+    if (token) fetch(`https://recipesage.com/api/users/sessioncheck?token=${token}`).then(response => {
+      if (!response.ok && response.status == 401) {
+        chrome.storage.local.set({ token: null });
+      }
+    });
+  });
+}
+
+setInterval(renewToken, 1000 * 60 * 60 * 24); // Once per 24 hours
+
+setTimeout(renewToken); // Avoid locking render loop
