41448d5da6b375cdfeba1f9f05d0d957e2428c0e
Julian Poyourow <julianpoyo@gmail.com>
2019-04-20 19:54:29 -0700

    Refactored recipe image file upload

--- Frontend/src/pages/recipe-components/edit-recipe/edit-recipe.ts
+++ Frontend/src/pages/recipe-components/edit-recipe/edit-recipe.ts
@@ -55,45 +55,56 @@ export class EditRecipePage {
     }
 
     let MAX_FILE_SIZE_MB = 8;
-    if (files[0].size / 1024 / 1024 > MAX_FILE_SIZE_MB) {
-      // Image is larger than MAX_FILE_SIZE_MB
+    let ENABLE_LOCAL_CONVERSIONS = true;
+    let isOverMaxSize = files[0].size / 1024 / 1024 > MAX_FILE_SIZE_MB; // Image is larger than MAX_FILE_SIZE_MB
+
+    if (!isOverMaxSize) {
+      // Image size is OK, upload the image directly for high quality server conversion
+      console.log(`Image is under ${MAX_FILE_SIZE_MB}MB`);
+      this.recipe.imageFile = files[0];
+      this.imageBlobURL = this.domSanitizationService.bypassSecurityTrustUrl(
+        (window.URL || (<any>window).webkitURL).createObjectURL(this.recipe.imageFile)
+      );
+    } else if (isOverMaxSize && ENABLE_LOCAL_CONVERSIONS) {
+      // Image is too large, do some resizing before high quality server conversion
+      console.log(`Image is over ${MAX_FILE_SIZE_MB}MB. Converting locally`);
+      var loadingImage = loadImage(
+        files[0],
+        (renderedCanvas, exif) => {
+          renderedCanvas.toBlob(myBlob => {
+            myBlob.name = this.recipe.imageFile.name;
+            this.recipe.imageFile = myBlob;
+            this.imageBlobURL = this.domSanitizationService.bypassSecurityTrustUrl(
+              (window.URL || (<any>window).webkitURL).createObjectURL(this.recipe.imageFile)
+            );
+
+            console.log('Local conversion complete');
+          }, 'image/jpeg', 1);
+        },
+        {
+          maxWidth: 800,
+          maxHeight: 800,
+          crop: true,
+          canvas: true,
+          orientation: true
+        }
+      );
+
+      loadingImage.onerror = err => {
+        // Image is too large and local conversion failed
+        console.log(`Image is over ${MAX_FILE_SIZE_MB}MB. Local conversion failed`);
+        this.toastCtrl.create({
+          message: `The max image file size is ${MAX_FILE_SIZE_MB}MB. Please select a smaller image.`,
+          duration: 6000
+        }).present();
+      };
+    } else {
+      // Image is too large and local conversions are not enabled
+      console.log(`Image is over ${MAX_FILE_SIZE_MB}MB. Local conversion not enabled`);
       this.toastCtrl.create({
         message: `The max image file size is ${MAX_FILE_SIZE_MB}MB. Please select a smaller image.`,
         duration: 6000
       }).present();
-      return
-    }
-
-    this.recipe.imageFile = files[0];
-    this.imageBlobURL = this.domSanitizationService.bypassSecurityTrustUrl(
-      (window.URL || (<any>window).webkitURL).createObjectURL(this.recipe.imageFile)
-    );
-
-    let ENABLE_LOCAL_CONVERSIONS = false;
-    if (ENABLE_LOCAL_CONVERSIONS) {
-      var loadingImage = loadImage(
-          files[0],
-          (renderedCanvas, exif) => {
-            renderedCanvas.toBlob(myBlob => {
-              myBlob.name = this.recipe.imageFile.name;
-              this.recipe.imageFile = myBlob;
-              this.imageBlobURL = this.domSanitizationService.bypassSecurityTrustUrl(
-                (window.URL || (<any>window).webkitURL).createObjectURL(this.recipe.imageFile)
-              );
-
-              console.log('Local conversion complete');
-            }, 'image/jpeg', 1);
-          },
-          {
-            maxWidth: 200,
-            canvas: true,
-            orientation: true
-          }
-      );
-
-      loadingImage.onerror = err => {
-        console.log('Local conversion failed', err)
-      };
     }
   }
 
