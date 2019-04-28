71f89966439abc0d7c927c20d32f148a8ec2bf48
Julian Poyourow <julianpoyo@gmail.com>
2019-04-28 11:54:28 -0700

    Filter headers from select ingredients

--- Frontend/src/components/select-ingredients/select-ingredients.ts
+++ Frontend/src/components/select-ingredients/select-ingredients.ts
@@ -47,7 +47,7 @@ export class SelectIngredientsComponent {
   }
 
   applyScale(init?) {
-    this.scaledIngredients = this.recipeService.scaleIngredients(this._ingredients, this.scale);
+    this.scaledIngredients = this.recipeService.scaleIngredients(this._ingredients, this.scale).filter(e => !e.isHeader);
 
     this.selectedIngredients = [];
     for (var i = 0; i < (this.scaledIngredients || []).length; i++) {
