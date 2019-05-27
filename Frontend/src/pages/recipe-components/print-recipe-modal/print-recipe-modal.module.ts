0db21cc16ac12ec15f5e1b61e6e9639d3d1ed2bd
Julian Poyourow <julianpoyo@gmail.com>
2019-05-27 14:26:38 -0700

    Use recipe preview component for print recipe modal

--- Frontend/src/pages/recipe-components/print-recipe-modal/print-recipe-modal.module.ts
+++ Frontend/src/pages/recipe-components/print-recipe-modal/print-recipe-modal.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { PrintRecipeModalPage } from './print-recipe-modal';
+import { ComponentsModule } from '../../../components/components.module';
 
 @NgModule({
   declarations: [
@@ -8,6 +9,7 @@ import { PrintRecipeModalPage } from './print-recipe-modal';
   ],
   imports: [
     IonicPageModule.forChild(PrintRecipeModalPage),
+    ComponentsModule
   ],
 })
 export class PrintRecipeModalPageModule {}
