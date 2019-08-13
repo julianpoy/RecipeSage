20878778cf7c5fe81a712734f0f35c46c88f4c7a
Julian Poyourow <julianpoyo@gmail.com>
2019-08-12 22:32:12 -0700

    I4: Add FormsModule to message-thread page

--- Frontend/src/app/pages/messaging-components/message-thread/message-thread.module.ts
+++ Frontend/src/app/pages/messaging-components/message-thread/message-thread.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { MessageThreadPage } from './message-thread.page';
 
@@ -17,7 +18,9 @@ import { MessageThreadPage } from './message-thread.page';
         path: '',
         component: MessageThreadPage
       }
-    ])
+    ]),
+    FormsModule,
+    ReactiveFormsModule
   ],
 })
 export class MessageThreadPageModule {}
