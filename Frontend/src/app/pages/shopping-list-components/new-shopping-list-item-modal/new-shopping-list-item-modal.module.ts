5e86ba850fd948a931b39f8faa204040da3a2385
Julian Poyourow <julianpoyo@gmail.com>
2019-08-13 18:50:36 -0700

    I4: Add missing imports for new shopping list item modal

--- Frontend/src/app/pages/shopping-list-components/new-shopping-list-item-modal/new-shopping-list-item-modal.module.ts
+++ Frontend/src/app/pages/shopping-list-components/new-shopping-list-item-modal/new-shopping-list-item-modal.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { NewShoppingListItemModalPage } from './new-shopping-list-item-modal.page';
+
+import { SelectRecipeModule } from '@/components/select-recipe/select-recipe.module';
+import { SelectIngredientsModule } from '@/components/select-ingredients/select-ingredients.module';
 
 @NgModule({
   declarations: [
@@ -11,6 +15,10 @@ import { NewShoppingListItemModalPage } from './new-shopping-list-item-modal.pag
   imports: [
     CommonModule,
     IonicModule,
+    FormsModule,
+    ReactiveFormsModule,
+    SelectRecipeModule,
+    SelectIngredientsModule
   ],
   entryComponents: [
     NewShoppingListItemModalPage,
