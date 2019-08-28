2f0286970e4c7d9b0f216947edc36c8c5d6a56ad
Julian Poyourow <julianpoyo@gmail.com>
2019-08-27 18:52:18 -0700

    I4: Switch to hash routing

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -119,7 +119,10 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, {
+      useHash: true,
+      preloadingStrategy: PreloadAllModules
+    })
   ],
   exports: [RouterModule]
 })
