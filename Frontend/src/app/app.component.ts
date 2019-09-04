364c7092eb2ac061386a9fcba45a06b582b9bc9e
Julian Poyourow <julianpoyo@gmail.com>
2019-09-03 22:26:23 -0700

    I4: Fix window reference for onSWUpdate

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -90,7 +90,7 @@ export class AppComponent {
       } catch (e) { }
     });
 
-    window.onSWUpdate = () => {
+    (window as any).onSWUpdate = () => {
       console.log('Update is waiting for pause...');
       if ((window as any).isHidden()) {
         (window as any).location.reload(true);
