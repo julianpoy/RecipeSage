0f4dd7b89dd52aef8951ffc23294e98e08275e6c
Julian Poyourow <julianpoyo@gmail.com>
2019-09-06 23:51:37 -0700

    v2 Beta: Remove hammerjs

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -6,8 +6,6 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
-import 'hammerjs';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { DefaultPageGuardService } from './services/default-page-guard.service';
