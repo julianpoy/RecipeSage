7a176455ee9bf9749af86fea78a2f81514074663
Julian Poyourow <julianpoyo@gmail.com>
2019-09-12 17:43:39 -0700

    Re-add sentry

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
@@ -7,10 +7,27 @@ import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { LoadingBarModule } from '@ngx-loading-bar/core';
+import * as Sentry from '@sentry/browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { DefaultPageGuardService } from './services/default-page-guard.service';
+
+Sentry.init({
+  release: (window as any).version,
+  dsn: 'https://056d11b20e624d52a5771ac8508dd0b8@sentry.io/1219200'
+});
+
+export class SentryErrorHandler extends ErrorHandler {
+  handleError(error) {
+    super.handleError(error);
+    try {
+      Sentry.captureException(error.originalError || error);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+}
 
 @NgModule({
   declarations: [
@@ -24,6 +41,7 @@ import { DefaultPageGuardService } from './services/default-page-guard.service';
     LoadingBarModule
   ],
   providers: [
+    { provide: ErrorHandler, useClass: SentryErrorHandler },
     StatusBar,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
