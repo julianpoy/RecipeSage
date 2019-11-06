ecb0a68e7451d2d42bc3a2a1fc43f401402d9e52
Julian Poyourow <julianpoyo@gmail.com>
2019-11-05 18:17:26 -0800

    Fix SQLite compat issues

--- Backend/migrations/20191026055147-remove-recipe-image-column.js
+++ Backend/migrations/20191026055147-remove-recipe-image-column.js
@@ -11,31 +11,33 @@ module.exports = {
         transaction
       });
 
-      const now = new Date();
+      if (recipes.length) { // Avoid generating emtpy SQL
+        const now = new Date();
 
-      const images = await queryInterface.bulkInsert('Images', recipes.map(recipe => ({
-        id: UUID.v4(),
-        userId: recipe.userId,
-        location: recipe.image.location,
-        key: recipe.image.key,
-        json: JSON.stringify(recipe.image),
-        createdAt: now,
-        updatedAt: now
-      })), {
-        transaction,
-        returning: true
-      });
+        const images = await queryInterface.bulkInsert('Images', recipes.map(recipe => ({
+          id: UUID.v4(),
+          userId: recipe.userId,
+          location: recipe.image.location,
+          key: recipe.image.key,
+          json: JSON.stringify(recipe.image),
+          createdAt: now,
+          updatedAt: now
+        })), {
+          transaction,
+          returning: true
+        });
 
-      await queryInterface.bulkInsert('Recipe_Images', images.map((image, idx) => ({
-        id: UUID.v4(),
-        recipeId: recipes[idx].id,
-        imageId: image.id,
-        order: 0,
-        createdAt: now,
-        updatedAt: now
-      })), {
-        transaction
-      });
+        await queryInterface.bulkInsert('Recipe_Images', images.map((image, idx) => ({
+          id: UUID.v4(),
+          recipeId: recipes[idx].id,
+          imageId: image.id,
+          order: 0,
+          createdAt: now,
+          updatedAt: now
+        })), {
+          transaction
+        });
+      }
 
       await queryInterface.removeColumn('Recipes', 'image', {
         transaction
@@ -52,19 +54,21 @@ module.exports = {
         transaction
       });
 
-      const [recipeImages] = await sequelize.query('SELECT "recipeId", "json" FROM "Recipe_Images" INNER JOIN "Images" on "Images".id = "Recipe_Images"."imageId" WHERE "Recipe_Images".order = 0', {
+      const [recipeImages] = await sequelize.query('SELECT "recipeId", "json" FROM "Recipe_Images" INNER JOIN "Images" on "Images".id = "Recipe_Images"."imageId" WHERE "Recipe_Images"."order" = 0', {
         transaction
       });
 
-      await Promise.all(recipeImages.map(async recipeImage => {
-        queryInterface.bulkUpdate('Recipes', {
-          image: recipeImage.json // Vals
-        }, {
-          id: recipeImage.recipeId // Where
-        }, {
-          transaction
-        });
-      }));
+      if (recipeImages.length) { // Avoid generating emtpy SQL
+        await Promise.all(recipeImages.map(async recipeImage => {
+          queryInterface.bulkUpdate('Recipes', {
+            image: recipeImage.json // Vals
+          }, {
+            id: recipeImage.recipeId // Where
+          }, {
+            transaction
+          });
+        }));
+      }
 
       await sequelize.query('DELETE FROM "Recipe_Images" WHERE true', {
         transaction
