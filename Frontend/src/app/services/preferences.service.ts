144eaf2b7261bb3d0a8913178240f2aa291c2702
Julian Poyourow <julianpoyo@gmail.com>
2019-12-07 14:26:49 -0800

    Use tile view as default view type

--- Frontend/src/app/services/preferences.service.ts
+++ Frontend/src/app/services/preferences.service.ts
@@ -64,9 +64,8 @@ export class PreferencesService {
     [MyRecipesPreferenceKey.ShowLabelChips]: false,
     [MyRecipesPreferenceKey.ShowImages]: true,
     [MyRecipesPreferenceKey.ShowSource]: false,
-    // TODO: Remove default list view after default settings go out
     // Show list by default on small screens
-    [MyRecipesPreferenceKey.ViewType]: 'list' || Math.min(window.innerWidth, window.innerHeight) < 440 ? 'list' : 'tiles',
+    [MyRecipesPreferenceKey.ViewType]: Math.min(window.innerWidth, window.innerHeight) < 440 ? 'list' : 'tiles',
     [MyRecipesPreferenceKey.SortBy]: '-title',
 
     [MealPlanPreferenceKey.ShowAddedBy]: false,
