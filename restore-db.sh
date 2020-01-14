b7c7eaa27f5933f196289d7b93e85e53878d0c1a
Julian Poyourow <julianpoyo@gmail.com>
2020-01-13 23:12:58 -0800

    Create db restore script

---
+++ restore-db.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+server=root@db1.kondeo.com
+scp -r $server:/pg-backup/$(ssh $server 'ls -t /pg-backup | head -1') tmp.sql
+
+docker-compose exec postgres dropdb -U $POSTGRES_USER $POSTGRES_DB
+docker-compose exec postgres createdb -U $POSTGRES_USER $POSTGRES_DB
+
+cat tmp.sql | docker exec -i "$(docker-compose ps -q postgres)" pg_restore -U $POSTGRES_USER -d $POSTGRES_DB
+
+rm tmp.sql
