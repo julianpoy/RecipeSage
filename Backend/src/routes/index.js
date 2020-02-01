f09e1687dc18a05923dffe32df8d05c9e3cb82bd
Julian Poyourow <julianpoyo@gmail.com>
2020-01-31 17:07:12 -0800

    Fix endless update warning for development version

--- Backend/src/routes/index.js
+++ Backend/src/routes/index.js
@@ -38,6 +38,8 @@ router.get('/versioncheck', (req, res, next) => {
     supported = semver.satisfies(version, MIN_SUPPORTED_FRONTEND_VERSION);
   }
 
+  if (req.query.version === 'development') supported = true;
+
   res.status(200).json({
     supported
   });
