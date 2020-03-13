df8700925079245cf484a25260efb3251b21a28e
Julian Poyourow <julianpoyo@gmail.com>
2020-03-13 14:40:47 -0700

    Confirm clearning pins

--- Frontend/src/app/components/cooking-toolbar/cooking-toolbar.component.ts
+++ Frontend/src/app/components/cooking-toolbar/cooking-toolbar.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from '@ionic/angular';
+import { AlertController, NavController } from '@ionic/angular';
 
 import { RouteMap } from '@/services/util.service';
 import { CookingToolbarService } from '@/services/cooking-toolbar.service';
@@ -12,10 +12,30 @@ import { CookingToolbarService } from '@/services/cooking-toolbar.service';
 export class CookingToolbarComponent {
   constructor(
     private navCtrl: NavController,
+    private alertCtrl: AlertController,
     private cookingToolbarService: CookingToolbarService
   ) {}
 
   openRecipe(recipeId: string) {
     this.navCtrl.navigateForward(RouteMap.RecipePage.getPath(recipeId));
+  }
+
+  async clearPins() {
+    const alert = await this.alertCtrl.create({
+      header: 'Clear All Pinned Recipes?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        },
+        {
+          text: 'Clear',
+          handler: () => {
+            this.cookingToolbarService.clearPins();
+          }
+        }
+      ]
+    });
+    alert.present();
   }
 }
