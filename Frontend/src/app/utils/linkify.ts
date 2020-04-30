b46bc103bbdb3f783215ff3029076430f452e8ba
Julian Poyourow <julianpoyo@gmail.com>
2020-04-30 10:11:58 -0700

    Linkify in recipe notes

---
+++ Frontend/src/app/utils/linkify.ts
@@ -0,0 +1,12 @@
+import * as LinkifyStr from 'linkifyjs/string';
+
+export const linkifyStr = (str: string) => {
+  return LinkifyStr(str, {
+    target: (href, type) => {
+      if (type !== 'url') return;
+      if (href.includes('recipesage.com')) return; // All recipesage.com should open in same tab
+      return '_blank';
+    },
+    className: 'linkified'
+  });
+}
