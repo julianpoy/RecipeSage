f2738e06eea28d05b63b43988b34befd02b987d1
Julian Poyourow <julianpoyo@gmail.com>
2020-06-06 08:44:29 -0700

    Use native datepicker for new meal plan item modal page

--- Frontend/src/app/pages/meal-plan-components/new-meal-plan-item-modal/new-meal-plan-item-modal.page.ts
+++ Frontend/src/app/pages/meal-plan-components/new-meal-plan-item-modal/new-meal-plan-item-modal.page.ts
@@ -11,14 +11,14 @@ import { UtilService } from '@/services/util.service';
 })
 export class NewMealPlanItemModalPage {
 
-  todayYear = new Date().getFullYear();
-
   @Input() isEditing = false;
   @Input() inputType = 'recipe';
   @Input() recipe;
   @Input() title: any = '';
   @Input() meal: any;
   @Input() scheduled = new Date();
+
+  sanitizedScheduled;
 
   constructor(
     public navCtrl: NavController,
@@ -27,10 +27,28 @@ export class NewMealPlanItemModalPage {
     public loadingService: LoadingService,
     public utilService: UtilService,
     public toastCtrl: ToastController) {
+
+    setTimeout(() => {
+      this.setSanitizedScheduled();
+    });
+  }
+
+  setSanitizedScheduled() {
+    const scheduled = new Date(this.scheduled);
+    const year = scheduled.getFullYear();
+    const month = (scheduled.getMonth() + 1).toString().padStart(2, '0');
+    const date = scheduled.getDate().toString().padStart(2, '0');
+
+    this.sanitizedScheduled = `${year}-${month}-${date}`;
   }
 
   scheduledDateChange(event) {
-    this.scheduled = event.detail.value;
+    const [year, month, date] = event.target.value.split('-');
+    const scheduled = new Date();
+    scheduled.setDate(date);
+    scheduled.setMonth(month - 1);
+    scheduled.setFullYear(year);
+    this.scheduled = scheduled;
   }
 
   isFormValid() {
