550dedc76277d2c51f2abfd26fbff1c4fbcbc4f5
Julian Poyourow <julianpoyo@gmail.com>
2020-11-30 10:44:33 -0800

    Add selfhost static image push

--- scripts/deploy/push_static_docker.sh
+++ scripts/deploy/push_static_docker.sh
@@ -2,12 +2,15 @@
 
 set -e
 
-docker build -f Frontend/circle.Dockerfile -t julianpoy/recipesage:static-latest --build-arg VERSION=$2 .
+docker build -f Frontend/circle.Dockerfile -t julianpoy/recipesage:static-latest -t julianpoy/recipesage-selfhost:static-latest --build-arg VERSION=$2 .
 
 docker push julianpoy/recipesage:static-latest
+docker push julianpoy/recipesage-selfhost:static-latest
 
 if ! [ -z "$2" ]
 then
   docker image tag julianpoy/recipesage:static-latest julianpoy/recipesage:static-$2
+  docker image tag julianpoy/recipesage-selfhost:static-latest julianpoy/recipesage-selfhost:static-$2
   docker push julianpoy/recipesage:static-$2
+  docker push julianpoy/recipesage-selfhost:static-$2
 fi
