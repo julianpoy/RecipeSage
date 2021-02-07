e43c89d064041549b8b26a3dd80a4eb1aa6ac0f1
Julian Poyourow <julianpoyo@gmail.com>
2021-02-07 11:42:29 -0800

    Validate session for ingredient-instruction-classifier proxy

--- Backend/src/routes/proxy.js
+++ Backend/src/routes/proxy.js
@@ -2,23 +2,27 @@ const express = require('express');
 const router = express.Router();
 const fetch = require('node-fetch');
 
-router.post('/ingredient-instruction-classifier', async (req, res, next) => {
-  try {
-    const response = await fetch(process.env.INGREDIENT_INSTRUCTION_CLASSIFIER_URL, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(req.body),
-    });
+router.post(
+  '/ingredient-instruction-classifier',
+  MiddlewareService.validateSession(['user']),
+  async (req, res, next) => {
+    try {
+      const response = await fetch(process.env.INGREDIENT_INSTRUCTION_CLASSIFIER_URL, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(req.body),
+      });
 
-    const json = await response.json();
+      const json = await response.json();
 
-    res.status(200).send(json);
-  } catch(e) {
-    next(e);
+      res.status(200).send(json);
+    } catch(e) {
+      next(e);
+    }
   }
-});
+);
 
 module.exports = router;
 
