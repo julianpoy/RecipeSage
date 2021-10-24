7c54357c4fb64521d825890dc2649cd8c909f6e0
Julian Poyourow <julianpoyo@gmail.com>
2021-10-24 15:05:40 -0700

    feat: Remove page-guard webshare target

--- Frontend/src/app/services/default-page-guard.service.ts
+++ Frontend/src/app/services/default-page-guard.service.ts
@@ -9,13 +9,8 @@ export class DefaultPageGuardService {
 
   canActivate() {
     const isLoggedIn = this.utilService.isLoggedIn();
-    const fullURL = new URL(window.location.href);
-    const isShareTarget = fullURL.pathname === "/shareTarget";
 
-    if (isLoggedIn) {
-      if (isShareTarget) this.navCtrl.navigateRoot(RouteMap.EditRecipePage.getPath('new') + '?' + fullURL.searchParams.toString());
-      else this.navCtrl.navigateRoot(RouteMap.HomePage.getPath('main'));
-    }
+    if (isLoggedIn) this.navCtrl.navigateRoot(RouteMap.HomePage.getPath('main'));
     else this.navCtrl.navigateRoot(RouteMap.WelcomePage.getPath());
 
     return false;
