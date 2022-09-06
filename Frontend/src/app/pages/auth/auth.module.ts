ce0c88c2a57fdfde62ea9a4f86f4fd82107647fc
Julian Poyourow <julian@poyourow.com>
2022-09-06 00:23:39 +0000

    fix: login modal, present on unauthorized

--- Frontend/src/app/pages/auth/auth.module.ts
+++ Frontend/src/app/pages/auth/auth.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { AuthPage } from './auth.page';
+import { LogoIconModule } from '@/components/logo-icon/logo-icon.module';
 import { TosClickwrapAgreementModule } from '@/components/tos-clickwrap-agreement/tos-clickwrap-agreement.module';
 
 import { GlobalModule } from '@/global.module';
@@ -22,6 +24,9 @@ import { GlobalModule } from '@/global.module';
       }
     ]),
     GlobalModule,
+    FormsModule,
+    ReactiveFormsModule,
+    LogoIconModule,
     TosClickwrapAgreementModule
   ],
 })
