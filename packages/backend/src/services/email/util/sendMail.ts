9d14e4414a3ee82322bf0824c385227ffcf696f3
Julian Poyourow <julian@poyourow.com>
2023-04-08 15:50:03 -0700

    fix: add default s3 region so AWSS3 doesn't throw

--- packages/backend/src/services/email/util/sendMail.ts
+++ packages/backend/src/services/email/util/sendMail.ts
@@ -2,7 +2,9 @@ import { SESClient, SendEmailCommand, SendEmailCommandInput } from '@aws-sdk/cli
 
 const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID || '';
 const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY || '';
-const AWS_REGION = process.env.AWS_REGION || '';
+const AWS_REGION = process.env.AWS_REGION || 'us-west-2';
+
+const ENABLE = AWS_ACCESS_KEY_ID && AWS_SECRET_ACCESS_KEY;
 
 const ses = new SESClient({
   region: AWS_REGION,
@@ -19,6 +21,8 @@ export const sendMail = async (
   html: string,
   plain: string
 ) => {
+  if (!ENABLE) return;
+
   const params: SendEmailCommandInput = {
     Destination: {
       CcAddresses: ccAddresses,
