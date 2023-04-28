2b0fa9ea51c7a5c283ed549394443a37201df410
Julian Poyourow <julian@poyourow.com>
2023-04-28 04:04:13 +0000

    fix: buildx setup

--- scripts/circleci_buildx.sh
+++ scripts/circleci_buildx.sh
@@ -16,6 +16,3 @@ docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 docker context create multi-arch-build
 docker buildx create --use multi-arch-build
 
-DIR="$(cd "$(dirname "$0")" && pwd)"
-sh $DIR/build_push_docker.sh $1
-
