7d991dd7bb9735c6d85601d26f55faa7f553ae9a
Julian Poyourow <julian@poyourow.com>
2023-05-08 05:10:42 +0000

    wip: prisma

--- packages/backend-trpc/src/procedures/recipes/getRecipes.ts
+++ packages/backend-trpc/src/procedures/recipes/getRecipes.ts
@@ -1,6 +1,6 @@
 import { publicProcedure, router } from '../../trpc';
 import { z } from 'zod';
-import {prisma} from 'src/dbHelpers/db';
+import { prisma } from '@recipesage/prisma';
 
 export const getRecipes = publicProcedure
   .input(z.object({
---
+++ packages/prisma/src/index.ts
@@ -0,0 +1,4 @@
+export * from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
+export const prisma = new PrismaClient();
+
