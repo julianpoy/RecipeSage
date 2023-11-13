beadcf1b51a4bb4f5bd0600d32e046a770477987
julianpoyourow <julian@poyourow.com>
2023-11-13 05:57:26 +0000

    chore: move assistant user init to prisma seeders

--- packages/trpc/src/services/chat/assistant.ts
+++ packages/trpc/src/services/chat/assistant.ts
@@ -14,47 +14,6 @@ import {
 import dedent from "ts-dedent";
 import { Capabilities, userHasCapability } from "../capabilities";
 
-// TODO: Move to seed lib
-const initAssistantUser = async () => {
-  const assistantUser = await prisma.user.upsert({
-    create: {
-      name: "RecipeSage Cooking Assistant",
-      email: "assistant@recipesage.com",
-      passwordHash: "nologin",
-      passwordSalt: "nologin",
-      passwordVersion: 2,
-      handle: "assistant",
-      enableProfile: true,
-    },
-    where: {
-      email: "assistant@recipesage.com",
-    },
-    update: {},
-  });
-
-  const existingProfileItem = await prisma.profileItem.findFirst({
-    where: {
-      userId: assistantUser.id,
-      order: 0,
-    },
-  });
-
-  if (!existingProfileItem) {
-    await prisma.profileItem.create({
-      data: {
-        userId: assistantUser.id,
-        type: "all-recipes",
-        title: "Created by RecipeSage Cooking Assistant",
-        visibility: "public",
-        order: 0,
-      },
-    });
-  }
-};
-if (process.env.NODE_ENV !== "test") {
-  initAssistantUser();
-}
-
 export class Assistant {
   private openAiHelper: OpenAIHelper;
   /**
