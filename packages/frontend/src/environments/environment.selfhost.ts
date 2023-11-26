79578a846c0d870dde8613a3f2c1260ce8a5cbea
julianpoyourow <julian@poyourow.com>
2023-11-26 18:01:49 +0000

    fix: subpath hosting for selfhost

--- packages/frontend/src/environments/environment.selfhost.ts
+++ packages/frontend/src/environments/environment.selfhost.ts
@@ -8,7 +8,7 @@ export const ENABLE_ANALYTICS = false;
 
 export const STRIPE_PK = "";
 
-export const API_BASE_URL = `${window.location.origin}/api/`;
+export const API_BASE_URL = "api/";
 export const GRIP_WS_URL = null;
 
 export const SENTRY_SAMPLE_RATE = 0;
