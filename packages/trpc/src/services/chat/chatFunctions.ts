bd803fc2107f609a1043bd031c8fc87cf6b7e00d
julianpoyourow <julian@poyourow.com>
2023-12-25 21:40:57 +0000

    fix: quotes in ingredient/instruction gpt response

--- packages/trpc/src/services/chat/chatFunctions.ts
+++ packages/trpc/src/services/chat/chatFunctions.ts
@@ -38,10 +38,12 @@ export const initBuildRecipe = (
         activeTime: typeof args.activeTime === "string" ? args.activeTime : "",
         totalTime: typeof args.totalTime === "string" ? args.totalTime : "",
         ingredients: Array.isArray(args.ingredients)
-          ? JSON.parse(`"${args.ingredients.join("\n")}`)
+          ? JSON.parse(`"${args.ingredients.join("\n").replaceAll('"', '\\"')}`)
           : "",
         instructions: Array.isArray(args.instructions)
-          ? JSON.parse(`"${args.instructions.join("\n")}`)
+          ? JSON.parse(
+              `"${args.instructions.join("\n").replaceAll('"', '\\"')}`,
+            )
           : "",
         notes: "",
       };
@@ -127,10 +129,12 @@ export const initOCRFormatRecipe = (
         activeTime: typeof args.activeTime === "string" ? args.activeTime : "",
         totalTime: typeof args.totalTime === "string" ? args.totalTime : "",
         ingredients: Array.isArray(args.ingredients)
-          ? JSON.parse(`"${args.ingredients.join("\n")}`)
+          ? JSON.parse(`"${args.ingredients.join("\n").replaceAll('"', '\\"')}`)
           : "",
         instructions: Array.isArray(args.instructions)
-          ? JSON.parse(`"${args.instructions.join("\n")}`)
+          ? JSON.parse(
+              `"${args.instructions.join("\n").replaceAll('"', '\\"')}`,
+            )
           : "",
         notes: typeof args.notes === "string" ? args.notes : "",
       };
