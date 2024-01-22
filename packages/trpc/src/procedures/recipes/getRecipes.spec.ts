b87c761b8f1279a810bca0c49d558e684b069904
Uliana Purtova <upurtova2@gmail.com>
2024-01-21 19:04:50 -0500

    test: unit tests on recipes

--- packages/trpc/src/procedures/recipes/getRecipes.spec.ts
+++ packages/trpc/src/procedures/recipes/getRecipes.spec.ts
@@ -1,11 +1,30 @@
-import { trpcSetup, tearDown } from "../../testutils";
+import { trpcSetup, tearDown, createTrpcClient } from "../../testutils";
 import { prisma } from "@recipesage/prisma";
 import { User } from "@prisma/client";
 import type { CreateTRPCProxyClient } from "@trpc/client";
 import type { AppRouter } from "../../index";
 import { faker } from "@faker-js/faker";
 
+function getRecipeStats(userId: string) {
+  return {
+    userId,
+    title: faker.string.alphanumeric(10),
+    description: faker.string.alphanumeric(10),
+    yield: faker.string.alphanumeric(10),
+    folder: "main",
+    activeTime: faker.string.alphanumeric(10),
+    totalTime: faker.string.alphanumeric(10),
+    source: faker.string.alphanumeric(10),
+    url: faker.string.alphanumeric(10),
+    notes: faker.string.alphanumeric(10),
+    ingredients: faker.string.alphanumeric(10),
+    instructions: faker.string.alphanumeric(10),
+    rating: faker.number.int({ min: 1, max: 5 }),
+  };
+}
+
 describe("getRecipes", () => {
+  jest.setTimeout(50000);
   let user: User;
   let trpc: CreateTRPCProxyClient<AppRouter>;
 
@@ -20,19 +39,7 @@ describe("getRecipes", () => {
   it("returns a list of recipes given no filters", async () => {
     await prisma.recipe.create({
       data: {
-        userId: user.id,
-        title: faker.string.alphanumeric(10),
-        description: faker.string.alphanumeric(10),
-        yield: faker.string.alphanumeric(10),
-        folder: "main",
-        activeTime: faker.string.alphanumeric(10),
-        totalTime: faker.string.alphanumeric(10),
-        source: faker.string.alphanumeric(10),
-        url: faker.string.alphanumeric(10),
-        notes: faker.string.alphanumeric(10),
-        ingredients: faker.string.alphanumeric(10),
-        instructions: faker.string.alphanumeric(10),
-        rating: faker.number.int({ min: 1, max: 5 }),
+        ...getRecipeStats(user.id),
       },
     });
 
@@ -45,5 +52,92 @@ describe("getRecipes", () => {
     });
 
     expect(typeof response.recipes[0].id).toBe("string");
+  });
+
+  it("filters by ratings", async () => {
+    await prisma.recipe.create({
+      data: {
+        ...getRecipeStats(user.id),
+        rating: 3,
+      },
+    });
+
+    await prisma.recipe.create({
+      data: {
+        ...getRecipeStats(user.id),
+        rating: 4,
+      },
+    });
+
+    const response = await trpc.recipes.getRecipes.query({
+      userIds: [user.id],
+      ratings: [3],
+      limit: 3,
+      folder: "main",
+      orderBy: "title",
+      orderDirection: "asc",
+      offset: 0,
+    });
+
+    expect(response.totalCount).toEqual(1);
+    expect(response.recipes[0].rating).toEqual(3);
+  });
+
+  it("filters by labels", async () => {
+    const label = await prisma.label.create({
+      data: {
+        title: faker.string.alphanumeric(10),
+        userId: user.id,
+        labelGroupId: null,
+      },
+    });
+
+    await prisma.recipe.create({
+      data: {
+        ...getRecipeStats(user.id),
+        title: "myTitle",
+        recipeLabels: {
+          createMany: {
+            data: [{ labelId: label.id }],
+          },
+        },
+        rating: faker.number.int({ min: 1, max: 5 }),
+      },
+    });
+
+    await prisma.recipe.create({
+      data: {
+        ...getRecipeStats(user.id),
+      },
+    });
+
+    const response = await trpc.recipes.getRecipes.query({
+      userIds: [user.id],
+      labels: [label.title],
+      limit: 3,
+      folder: "main",
+      orderBy: "title",
+      orderDirection: "asc",
+      offset: 0,
+    });
+
+    expect(response.totalCount).toEqual(1);
+    expect(response.recipes[0].title).toEqual("myTitle");
+  });
+});
+
+describe("getRecipes - error cases", () => {
+  jest.setTimeout(50000);
+  it("invalid session", async () => {
+    const trpcNotLoggedIn = await createTrpcClient("12345");
+    expect(async () => {
+      await trpcNotLoggedIn.recipes.getRecipes.query({
+        limit: 3,
+        folder: "main",
+        orderBy: "title",
+        orderDirection: "asc",
+        offset: 0,
+      });
+    }).rejects.toThrow("Must pass userIds or be logged in");
   });
 });
