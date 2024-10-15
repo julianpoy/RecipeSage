b4bad91bc69cb7679b2fb17fb5443defc2cabb76
Uliana Purtova <upurtova2@gmail.com>
2024-10-15 17:25:15 -0400

    label group tests

---
+++ packages/trpc/src/procedures/labelGroups/createLabelGroup.spec.ts
@@ -0,0 +1,88 @@
+import { trpcSetup, tearDown } from "../../testutils";
+import { prisma } from "@recipesage/prisma";
+import { User } from "@prisma/client";
+import type { CreateTRPCProxyClient } from "@trpc/client";
+import type { AppRouter } from "../../index";
+
+describe("create Label", () => {
+  let user: User;
+  let user2: User;
+  let trpc: CreateTRPCProxyClient<AppRouter>;
+
+  beforeAll(async () => {
+    ({ user, user2, trpc } = await trpcSetup());
+  });
+
+  afterAll(() => {
+    return tearDown(user.id, user2.id);
+  });
+
+  describe("success", () => {
+    it("creates a label group with all parameters provided", async () => {
+      const label = await prisma.label.create({
+        data: {
+          userId: user.id,
+          title: "meat",
+        },
+      });
+      const labelGroup = await trpc.labelGroups.createLabelGroup.mutate({
+        title: "soup",
+        labelIds: [label.id],
+        warnWhenNotPresent: true,
+      });
+      expect(labelGroup.title).toEqual("soup");
+      const response = await prisma.labelGroup.findFirst({
+        where: {
+          id: labelGroup.id,
+        },
+      });
+      expect(typeof response?.id).toBe("string");
+    });
+  });
+
+  it("creates a label group with two labels parameters provided", async () => {
+    const label = await prisma.label.create({
+      data: {
+        userId: user.id,
+        title: "meat",
+      },
+    });
+    const label2 = await prisma.label.create({
+      data: {
+        userId: user2.id,
+        title: "fish",
+      },
+    });
+    const labelGroup = await trpc.labelGroups.createLabelGroup.mutate({
+      title: "soup",
+      labelIds: [label.id, label2.id],
+      warnWhenNotPresent: true,
+    });
+    expect(labelGroup.title).toEqual("soup");
+    const response = await prisma.labelGroup.findFirst({
+      where: {
+        id: labelGroup.id,
+      },
+    });
+    expect(typeof response?.id).toBe("string");
+  });
+
+  describe("error", () => {
+    it.only("fails to create a label group with not existing label", async () => {
+       await prisma.labelGroup.create({
+        data: {
+          userId: user.id,
+          title: "soup",
+          warnWhenNotPresent: true,
+        },
+      });
+      return expect(async () => {
+        await trpc.labelGroups.createLabelGroup.mutate({
+          title: "soup",
+          labelIds: ["00008495-d189-4a99-98bb-8888442de945"],
+          warnWhenNotPresent: true,
+        });
+      }).rejects.toThrow("Conflicting labelGroup title");
+    });
+  });
+});
---
+++ packages/trpc/src/procedures/labelGroups/deleteme.js
@@ -0,0 +1,64 @@
+2024-10-10 12:53:46 Setting up watches.  Beware: since -r was given, this may take a while!
+2024-10-10 12:53:47 Watches established.
+2024-10-10 12:53:52 Invalid Sentry Dsn: 1234
+2024-10-10 12:53:57 Error: Cannot find module 'showdown'
+2024-10-10 12:53:57 Require stack:
+2024-10-10 12:53:57 - /app/packages/util/server/src/ml/assistant.ts
+2024-10-10 12:53:57 - /app/packages/util/server/src/ml/index.ts
+2024-10-10 12:53:57 - /app/packages/trpc/src/procedures/assistant/sendAssistantMessage.ts
+2024-10-10 12:53:57 - /app/packages/trpc/src/index.ts
+2024-10-10 12:53:57 - /app/packages/backend/src/app.ts
+2024-10-10 12:53:57 - /app/packages/backend/src/bin/www.ts
+2024-10-10 12:53:57     at Function.Module._resolveFilename (node:internal/modules/cjs/loader:1143:15)
+2024-10-10 12:53:57     at Function.Module._resolveFilename.sharedData.moduleResolveFilenameHook.installedValue (/app/node_modules/@cspotcode/source-map-support/source-map-support.js:811:30)
+2024-10-10 12:53:57     at Function.Module._resolveFilename (/app/node_modules/tsconfig-paths/src/register.ts:115:36)
+2024-10-10 12:53:57     at Function.Module._load (node:internal/modules/cjs/loader:984:27)
+2024-10-10 12:53:57     at Module.require (node:internal/modules/cjs/loader:1231:19)
+2024-10-10 12:53:57     at require (node:internal/modules/helpers:179:18)
+2024-10-10 12:53:57     at Object.<anonymous> (/app/packages/util/server/src/ml/assistant.ts:14:19)
+2024-10-10 12:53:57     at Module._compile (node:internal/modules/cjs/loader:1369:14)
+2024-10-10 12:53:57     at Module.m._compile (/app/node_modules/ts-node/src/index.ts:1618:23)
+2024-10-10 12:53:57     at Module._extensions..js (node:internal/modules/cjs/loader:1427:10) {
+2024-10-10 12:53:57   code: 'MODULE_NOT_FOUND',
+2024-10-10 12:53:57   requireStack: [
+2024-10-10 12:53:57     '/app/packages/util/server/src/ml/assistant.ts',
+2024-10-10 12:53:57     '/app/packages/util/server/src/ml/index.ts',
+2024-10-10 12:53:57     '/app/packages/trpc/src/procedures/assistant/sendAssistantMessage.ts',
+2024-10-10 12:53:57     '/app/packages/trpc/src/index.ts',
+2024-10-10 12:53:57     '/app/packages/backend/src/app.ts',
+2024-10-10 12:53:57     '/app/packages/backend/src/bin/www.ts'
+2024-10-10 12:53:57   ]
+2024-10-10 12:53:57 }
+2024-10-10 13:07:05 sh: can't kill pid 8: No such process
+2024-10-10 13:07:05 Setting up watches.  Beware: since -r was given, this may take a while!
+2024-10-10 13:07:07 Watches established.
+2024-10-10 13:07:10 Invalid Sentry Dsn: 1234
+2024-10-10 13:07:15 Error: Cannot find module 'showdown'
+2024-10-10 13:07:15 Require stack:
+2024-10-10 13:07:15 - /app/packages/util/server/src/ml/assistant.ts
+2024-10-10 13:07:15 - /app/packages/util/server/src/ml/index.ts
+2024-10-10 13:07:15 - /app/packages/trpc/src/procedures/assistant/sendAssistantMessage.ts
+2024-10-10 13:07:15 - /app/packages/trpc/src/index.ts
+2024-10-10 13:07:15 - /app/packages/backend/src/app.ts
+2024-10-10 13:07:15 - /app/packages/backend/src/bin/www.ts
+2024-10-10 13:07:15     at Function.Module._resolveFilename (node:internal/modules/cjs/loader:1143:15)
+2024-10-10 13:07:15     at Function.Module._resolveFilename.sharedData.moduleResolveFilenameHook.installedValue (/app/node_modules/@cspotcode/source-map-support/source-map-support.js:811:30)
+2024-10-10 13:07:15     at Function.Module._resolveFilename (/app/node_modules/tsconfig-paths/src/register.ts:115:36)
+2024-10-10 13:07:15     at Function.Module._load (node:internal/modules/cjs/loader:984:27)
+2024-10-10 13:07:15     at Module.require (node:internal/modules/cjs/loader:1231:19)
+2024-10-10 13:07:15     at require (node:internal/modules/helpers:179:18)
+2024-10-10 13:07:15     at Object.<anonymous> (/app/packages/util/server/src/ml/assistant.ts:14:19)
+2024-10-10 13:07:15     at Module._compile (node:internal/modules/cjs/loader:1369:14)
+2024-10-10 13:07:15     at Module.m._compile (/app/node_modules/ts-node/src/index.ts:1618:23)
+2024-10-10 13:07:15     at Module._extensions..js (node:internal/modules/cjs/loader:1427:10) {
+2024-10-10 13:07:15   code: 'MODULE_NOT_FOUND',
+2024-10-10 13:07:15   requireStack: [
+2024-10-10 13:07:15     '/app/packages/util/server/src/ml/assistant.ts',
+2024-10-10 13:07:15     '/app/packages/util/server/src/ml/index.ts',
+2024-10-10 13:07:15     '/app/packages/trpc/src/procedures/assistant/sendAssistantMessage.ts',
+2024-10-10 13:07:15     '/app/packages/trpc/src/index.ts',
+2024-10-10 13:07:15     '/app/packages/backend/src/app.ts',
+2024-10-10 13:07:15     '/app/packages/backend/src/bin/www.ts'
+2024-10-10 13:07:15   ]
+2024-10-10 13:07:15 }
+2024-10-10 13:07:05 ./packages/trpc/src/procedures/recipes/ ATTRIB createRecipe.spec.ts
