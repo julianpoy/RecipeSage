db9d66e4fdc2e7290cd5192261db1903c9c98fcc
julianpoyourow <julian@poyourow.com>
2025-02-01 23:23:14 +0000

    feat: getSimilarRecipes route cache

--- packages/frontend/src/app/utils/serviceWorker/routes/recipes/index.ts
+++ packages/frontend/src/app/utils/serviceWorker/routes/recipes/index.ts
@@ -1,5 +1,6 @@
 export { registerGetRecipesRoute } from "./registerGetRecipesRoute";
 export { registerGetRecipeRoute } from "./registerGetRecipeRoute";
+export { registerGetSimilarRecipesRoute } from "./registerGetSimilarRecipesRoute";
 export { registerSearchRecipesRoute } from "./registerSearchRecipesRoute";
 export { registerUpdateRecipeRoute } from "./registerUpdateRecipeRoute";
 export { registerCreateRecipeRoute } from "./registerCreateRecipeRoute";
