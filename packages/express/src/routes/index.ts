2ff0be0a0784fac8cc5de37ae35cd74d685e9eeb
julianpoyourow <julian@poyourow.com>
2025-06-17 04:25:28 +0000

    chore: add metrics endpoint

--- packages/express/src/routes/index.ts
+++ packages/express/src/routes/index.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import { importRouter } from "./import";
 import { mealPlansRouter } from "./mealPlans";
+import { metricsRouter } from "./metrics";
 import { wsRouter } from "./ws";
 
 const router = express.Router();
 
 router.use("/import", importRouter);
 router.use("/mealplans", mealPlansRouter);
+router.use("/metrics", metricsRouter);
 router.use("/ws", wsRouter);
 
 export { router as typesafeExpressIndexRouter };
