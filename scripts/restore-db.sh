1249696f1e08995978db1cc6d2d1363795164940
Julian Poyourow <julian@poyourow.com>
2025-11-23 19:46:33 -0800

    chore: update restore-db.sh

--- scripts/restore-db.sh
+++ scripts/restore-db.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
-
 POSTGRES_USER=recipesage_dev
 POSTGRES_DB=recipesage_dev
 
 if ! [ -z "$1" ]
 then
-    cp $1 restore.sql
+    cp $1 restore.sql.gz
 else
-    server=root@db1.recipesage.com
-    scp -r $server:/pg-backup/$(ssh $server 'ls -t /pg-backup | head -1') restore.sql
+    server=root@db3.recipesage.com
+    scp -r $server:/pg-backup/$(ssh $server 'ls -t /pg-backup | head -1') restore.sql.gz
 fi
 
 docker compose exec postgres psql -U $POSTGRES_USER -d $POSTGRES_DB -c 'SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pid <> pg_backend_pid();'
 docker compose exec postgres dropdb -U $POSTGRES_USER $POSTGRES_DB
 docker compose exec postgres createdb -U $POSTGRES_USER $POSTGRES_DB
 
-cat restore.sql | docker exec -i "$(docker compose ps -q postgres)" pg_restore -U $POSTGRES_USER -d $POSTGRES_DB
+gunzip -c restore.sql.gz | docker exec -i "$(docker compose ps -q postgres)" psql -U $POSTGRES_USER -d $POSTGRES_DB
+
+# Clean up
+rm restore.sql.gz
