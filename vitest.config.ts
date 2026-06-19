172fa4951205b871fd6fd3d0d8f7f47a88cb56b5
julianpoyourow <julian@poyourow.com>
2026-06-19 21:46:38 +0000

    chore: upgrade nx

---
+++ vitest.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    projects: [
+      "**/vite.config.{mjs,js,ts,mts}",
+      "**/vitest.config.{mjs,js,ts,mts}",
+    ],
+  },
+});
