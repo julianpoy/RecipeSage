d7e81d3a4c7033b96552abc60494a0e17e0cbbbc
julianpoyourow <julian@poyourow.com>
2026-08-11 02:22:49 +0000

    fix: selfhost api startup

--- scripts/selfhost-startup.sh
+++ scripts/selfhost-startup.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
+echo "==== Running migrations, please wait... ===="
 pnpm exec prisma migrate deploy
+
+echo "==== Running database seeders, please wait... ===="
 pnpm exec nx seed prisma
-pnpm exec ts-node --swc --project packages/cli/tsconfig.json packages/cli/src/main.ts indexRecipes
-pnpm exec ts-node --swc --project packages/backend/tsconfig.json packages/backend/src/main.ts
+
+echo "==== Rebuilding search index, please wait... ===="
+node dist/apps/cli/main.cjs indexRecipes
+
+echo "==== Starting API ===="
+node dist/apps/backend/main.cjs
 
